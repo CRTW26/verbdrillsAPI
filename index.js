22f6b69730638c56342617042d8dc9b30c3cba04
CRTW26 <worthington.connor@gmail.com>
2021-02-03 12:04:19 +0000

    before/afterEach added for testing listening on 3001

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/groups', function(req, res) {
-    res.send('Hello world');
+    res.send('Bye ');
 });
+
 
 module.exports = app;
--- tests/api.test.js
+++ tests/api.test.js
@@ -1,12 +1,23 @@
+const app = require('../index.js');
+const supertest = require('supertest');
+const request = supertest(app);
+
 describe('Testing API routes', () => {
 
-    const app = require('../index.js');
-    const supertest = require('supertest');
-    const request = supertest(app);
+    let server; 
+
+    beforeEach(() => {
+        server = app.listen(3001);
+    })
+
+    afterEach(async () => {
+        await server.close();
+    })
 
     test('/groups', async ()  => {
         const response = await request.get('/groups');
         expect(response.status).toBe(200);
+        expect(response.type).toBe
     });
 
 })
