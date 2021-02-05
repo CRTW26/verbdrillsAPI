f40441cfdd4cb2d052435703c05ad52ecd8c3c33
CRTW26 <worthington.connor@gmail.com>
2021-02-05 15:16:28 +0000

    Configures jest coverage

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ import { Router }  from 'express';
 import db from '../db';
 let router = Router();
 
-router.get('/hello/', (req,res) => {
-    res.send('hello');
-});
+// router.get('/hello/', (req,res) => {
+//     res.send('hello');
+// });
 
 router.get('/', async (req, res) => {
     const data = await db.query("SELECT * FROM users");
