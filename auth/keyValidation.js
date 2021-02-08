bc31f582fe5f586472009e23922c403b47d14221
CRTW26 <worthington.connor@gmail.com>
2021-02-08 11:33:08 +0000

    CREATES and DROPS tables after each test in key validation

--- auth/keyValidation.js
+++ auth/keyValidation.js
@@ -3,7 +3,7 @@ import db from '../db';
 const keyValidation = async (req, res, next) => {
     const apiKey = req.query.key;
     const keys = await db.query("SELECT key FROM users");
-    const keyArr = keys.rows.map(x => x.key);
+    const keyArr = await keys.rows.map(x => x.key);
 
     if (!apiKey) {
         return res.status(400).send({
--- tests/authTests/keyVaidation.test.js
+++ tests/authTests/keyVaidation.test.js
@@ -1,52 +1,44 @@
-// process.env.NODE_ENV = 'test';
-// const app = require('../../index.js');
-// const supertest = require('supertest');
-// const request = supertest(app);
-// import db from '../../db';
+process.env.NODE_ENV = 'test';
+const app = require('../../index.js');
+const supertest = require('supertest');
+const request = supertest(app);
+import db from '../../db';
 
-// describe('key validtion', () => {
+describe('key validtion', () => {
+
+    beforeEach(async () => {
+        //Create verb groups table
+        await db.query("CREATE TABLE groups (tense VARCHAR(30), type VARCHAR(15), list json)");
+        await db.query("INSERT INTO groups (tense, type, list) VALUES ('present', 'regular', '{\"regular\": {\"hablar\": [\"hablo\"]}}')")
+        //Create users table
+        await db.query("CREATE TABLE users (ID SERIAL PRIMARY KEY, username VARCHAR(30), password VARCHAR(30), key VARCHAR(5))");
+        await db.query("INSERT INTO users (username, password, key) VALUES ('user1', 'password', 'abc')");
+        await db.query("INSERT INTO users (username, password, key) VALUES ('user2', 'password', 'def')");
+    });
+
+    afterEach(async () => {
+        await db.query("DROP TABLE users");
+        await db.query("DROP TABLE groups");
+    });
+
+    afterAll(async () => {
+        db.end();
+    });
     
-//     beforeAll(async () => {
-//         await db.query("CREATE TABLE groups (tense VARCHAR(30), type VARCHAR(15), list json)");
-//         await db.query("CREATE TABLE users (ID SERIAL PRIMARY KEY, username VARCHAR(30), password VARCHAR(30), key VARCHAR(10))");
-//     });
+    test('/verbgroups/?tense=present&type=regular&key=abc', async () => {
+        const response = await request.get('/verbgroups/?tense=present&type=regular&key=abc');
+        expect(response.statusCode).toBe(200);
+    });
 
-//     beforeEach(async () => {
-//         //Create verb groups table
-//         // await db.query("CREATE TABLE groups (tense VARCHAR(30), type VARCHAR(15), list json)");
-//         await db.query("INSERT INTO groups (tense, type, list) VALUES ('present', 'regular', '{\"regular\": {\"hablar\": [\"hablo\"]}}')")
-//         //Create users table
-//         // await db.query("CREATE TABLE users (ID SERIAL PRIMARY KEY, username VARCHAR(30), password VARCHAR(30), key VARCHAR(5))");
-//         await db.query("INSERT INTO users (username, password, key) VALUES ('user1', 'password', 'abc')");
-//         await db.query("INSERT INTO users (username, password, key) VALUES ('user2', 'password', 'def')");
-//     });
+    test('/verbgroups/?tense=present&type=regular', async () => {
+        const response = await request.get('/verbgroups/?tense=present&type=regular');
+        expect(response.statusCode).toBe(400);
+        expect(response.body.response).toEqual("Missing API key");
+    });
 
-//     afterEach(async () => {
-//         // await db.query('DELETE FROM users');
-//         await db.query("TRUNCATE TABLE groups");
-//         await db.query("TRUNCATE TABLE users");
-//     });
-
-//     afterAll(async () => {
-//         await db.query("DROP TABLE users");
-//         await db.query("DROP TABLE groups");
-//         db.end();
-//     });
-    
-//     test('/verbgroups/?tense=present&type=regular&key=abc', async () => {
-//         const response = await request.get('/verbgroups/?tense=present&type=regular&key=abc');
-//         expect(response.statusCode).toBe(200);
-//     });
-
-//     test('/verbgroups/?tense=present&type=regular', async () => {
-//         const response = await request.get('/verbgroups/?tense=present&type=regular');
-//         expect(response.statusCode).toBe(400);
-//         expect(response.body.response).toEqual("Missing API key");
-//     });
-
-//     test('/verbgroups.?tense=present&type=regular&key=def', async () => {
-//         const response = await request.get('/verbgroups/?tense=present&type=regular&key=ghi');
-//         expect(response.statusCode).toBe(400);
-//         expect(response.body.response).toEqual("Invalid API key");
-//     });
-// })
+    test('/verbgroups.?tense=present&type=regular&key=def', async () => {
+        const response = await request.get('/verbgroups/?tense=present&type=regular&key=ghi');
+        expect(response.statusCode).toBe(400);
+        expect(response.body.response).toEqual("Invalid API key");
+    });
+})
