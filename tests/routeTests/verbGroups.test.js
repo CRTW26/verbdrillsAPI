9ec74ded5bfabc0b2e57f211e31c29b8dd10713f
CRTW26 <worthington.connor@gmail.com>
2021-02-08 11:29:51 +0000

    Removes comments from verbgroup testing

--- tests/routeTests/verbGroups.test.js
+++ tests/routeTests/verbGroups.test.js
@@ -1,59 +1,44 @@
-// process.env.NODE_ENV = 'test';
-// const app = require('../../index.js');
-// const supertest = require('supertest');
-// const request = supertest(app);
-// import db from '../../db';
+process.env.NODE_ENV = 'test';
+const app = require('../../index.js');
+const supertest = require('supertest');
+const request = supertest(app);
+import db from '../../db';
 
-// describe('verbGroups routes', () => {
+describe('verbGroups routes', () => {
 
-//     // let server; 
+    beforeEach(async () => {
+        await db.query("CREATE TABLE groups (tense VARCHAR(30), type VARCHAR(15), list json)");
+        await db.query("INSERT INTO groups (tense, type, list) VALUES ('present', 'regular', '{\"regular\": {\"hablar\": [\"hablo\"]}}')")
+        await db.query("INSERT INTO groups (tense, type, list) VALUES ('preterite', 'regular', '{\"regular\": {\"hablar\": [\"hable\"]}}')")
+        await db.query("INSERT INTO groups (tense, type, list) VALUES ('imperfect', 'regular', '{\"regular\": {\"hablar\": [\"hablaba\"]}}')")
 
-//     // beforeEach(() => {
-//     //     server = app.listen(3001);
-//     // })
+        await db.query("CREATE TABLE users (ID SERIAL PRIMARY KEY, username VARCHAR(30), password VARCHAR(30), key VARCHAR(5))");
+        await db.query("INSERT INTO users (username, password, key) VALUES ('user1', 'password', 'abc')");
+        await db.query("INSERT INTO users (username, password, key) VALUES ('user2', 'password', 'def')");
+    });
 
-//     // afterEach(async () => {
-//     //     db.end
-//     //     await server.close();
-//     // })
+    afterEach(async () => {
+        await db.query('DROP TABLE groups');
+        await db.query("DROP TABLE users");
+    });
 
-//     // beforeAll(async () => {
-//     //     await db.query("CREATE TABLE groups (tense VARCHAR(30), type VARCHAR(15), list json)");
-//     // });
+    afterAll(async () => {
+        db.end();
+    });
 
-//     beforeEach(async () => {
-//         await db.query("CREATE TABLE groups (tense VARCHAR(30), type VARCHAR(15), list json)");
-//         await db.query("INSERT INTO groups (tense, type, list) VALUES ('present', 'regular', '{\"regular\": {\"hablar\": [\"hablo\"]}}')")
-//         await db.query("INSERT INTO groups (tense, type, list) VALUES ('preterite', 'regular', '{\"regular\": {\"hablar\": [\"hable\"]}}')")
-//         await db.query("INSERT INTO groups (tense, type, list) VALUES ('imperfect', 'regular', '{\"regular\": {\"hablar\": [\"hablaba\"]}}')")
+    test('/verbgroups/?tense=present&type=regular', async () => {
+        const response = await request.get('/verbgroups/?tense=present&type=regular&key=abc');
+        expect(response.body.regular.hablar[0]).toEqual('hablo');
+    });
 
-//         await db.query("CREATE TABLE users (ID SERIAL PRIMARY KEY, username VARCHAR(30), password VARCHAR(30), key VARCHAR(5))");
-//         await db.query("INSERT INTO users (username, password, key) VALUES ('user1', 'password', 'abc')");
-//         await db.query("INSERT INTO users (username, password, key) VALUES ('user2', 'password', 'def')");
-//     });
+    test('/verbgroups/?tense=preterite&type=regular', async () => {
+        const response = await request.get('/verbgroups/?tense=preterite&type=regular&key=abc');
+        expect(response.body.regular.hablar[0]).toEqual('hable');
+    });
 
-//     afterEach(async () => {
-//         await db.query('DROP TABLE groups');
-//         await db.query("DROP TABLE users");
-//     });
+    test('/verbgroups/?tense=imperfect&type=regular', async () => {
+        const response = await request.get('/verbgroups/?tense=imperfect&type=regular&key=abc');
+        expect(response.body.regular.hablar[0]).toEqual('hablaba');
+    });
 
-//     afterAll(async () => {
-//         db.end();
-//     });
-
-//     test('/verbgroups/?tense=present&type=regular', async () => {
-//         const response = await request.get('/verbgroups/?tense=present&type=regular&key=abc');
-//         expect(response.body.regular.hablar[0]).toEqual('hablo');
-//     });
-
-//     test('/verbgroups/?tense=preterite&type=regular', async () => {
-//         const response = await request.get('/verbgroups/?tense=preterite&type=regular&key=abc');
-//         expect(response.body.regular.hablar[0]).toEqual('hable');
-//     });
-
-//     test('/verbgroups/?tense=imperfect&type=regular', async () => {
-//         const response = await request.get('/verbgroups/?tense=imperfect&type=regular&key=abc');
-//         expect(response.body.regular.hablar[0]).toEqual('hablaba');
-//     });
-
-// })
+})
