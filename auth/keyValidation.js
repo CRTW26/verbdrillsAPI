25d8f13f0af0dbb473cbda811bef4a7c6ba3d536
CRTW26 <worthington.connor@gmail.com>
2021-02-08 14:02:06 +0000

    Adds flag for sequential running of tests

--- auth/keyValidation.js
+++ auth/keyValidation.js
@@ -1,10 +1,6 @@
 import db from '../db';
 
 const keyValidation = async (req, res, next) => {
-    // console.log(req.query.key)
-    // console.log(String(req.query.key));
-    // const query = `SELECT key FROM users WHERE key=${req.query.key}`
-    // console.log(query)
     const apiKey = req.query.key;
     const keys = await db.query("SELECT key FROM users WHERE key=$1", [req.query.key]);
     // console.log(keys.rows.length);
