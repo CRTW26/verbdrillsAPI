4b10eba103a8186f1f784e086a33ef1a462250a4
CRTW26 <worthington.connor@gmail.com>
2021-02-10 13:19:17 +0000

    Adds start script to package.json

--- routes/verbGroups.js
+++ routes/verbGroups.js
@@ -3,9 +3,9 @@ import { Router } from 'express';
 import db from '../db';
 let router = Router();
 
-// router.get('/hello/', (req,res) => {
-//     res.send('hello');
-// });
+router.get('/hello/', (req,res) => {
+    res.send('hello');
+});
 
 router.get('/', keyValidation, async (req, res) => {
     const data = await db.query("SELECT list FROM groups WHERE tense=$1 AND type=$2", 
