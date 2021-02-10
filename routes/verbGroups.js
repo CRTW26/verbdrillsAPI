a087dd0342f65665f8b2f56dadb73866977a376a
CRTW26 <worthington.connor@gmail.com>
2021-02-10 10:16:09 +0000

    Has queries for databases in verbGroups and reference

--- routes/verbGroups.js
+++ routes/verbGroups.js
@@ -7,10 +7,12 @@ let router = Router();
 //     res.send('hello');
 // });
 
-router.get('/', keyValidation, async (req, res) => {
-    const data = await db.query("SELECT list FROM groups WHERE tense=$1 AND type=$2", 
-    [req.query.tense, req.query.type]);
-    return res.json(data.rows[0].list);
+router.get('/', async (req, res) => {
+    // const data = await db.query("SELECT list FROM groups WHERE tense=$1 AND type=$2", 
+    // [req.query.tense, req.query.type]);
+    // return res.json(data.rows[0].list);
+    const data = require(`../verbGroups/${req.query.type}/${req.query.tense}`);
+    res.json(data);
 });
 
 export default router; 
