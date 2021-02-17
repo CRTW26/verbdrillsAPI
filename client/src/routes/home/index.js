8b77e2bcc352e2b9bb9b761cc387e59f3c4c3795
CRTW26 <worthington.connor@gmail.com>
2021-02-17 11:31:15 +0000

    Adds homepage

--- client/src/routes/home/index.js
+++ client/src/routes/home/index.js
@@ -3,8 +3,18 @@ import style from './style.css';
 
 const Home = () => (
 	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
+		<h1>verbdrills API</h1>
+		<p>
+            The verdrills API provides access to one of the largest databases of conjugated Spanish verbs,
+            as well as lists of verbs grouped according to type and tense.
+        </p>
+        <p>
+            To view the available endpoints and see a sample response <a href='endpoints'>click here</a>. 
+        </p>
+        <p>
+            To access the API you must sign-up for an account and provide a description of your usage to generate 
+            an API key. 
+        </p>
 	</div>
 );
 
