1104b8002a06184ca2dec8e7a45489c2c82fdd53
CRTW26 <worthington.connor@gmail.com>
2021-02-17 14:27:36 +0000

    Adds login form

--- client/src/routes/login/index.js
+++ client/src/routes/login/index.js
@@ -2,8 +2,14 @@ import { h } from 'preact';
 import style from './style.css';
 
 const Login = () => (
-    <div>
-        <h1>Login</h1>
+    <div class={style.login}>
+        <form> 
+            <h1>Login</h1>
+            <input type="email" placeholder="Email" required></input>
+            <input type="password" placeholder="Password" required></input>
+            <input type="submit" value="Login" />
+            <a href="/signup"><p>Not a user?</p></a>
+        </form>
     </div>
 );
 
