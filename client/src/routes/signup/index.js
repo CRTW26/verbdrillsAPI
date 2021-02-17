c8a43e708117d3d47ae933047757ff7c08dae53c
CRTW26 <worthington.connor@gmail.com>
2021-02-17 15:21:50 +0000

    adds submit handler to signup

--- client/src/routes/signup/index.js
+++ client/src/routes/signup/index.js
@@ -1,16 +1,26 @@
 import { h } from 'preact';
 import style from './style.css';
 
-const Signup = () => (
-    <div class={style.signup}>
-        <form>
-        <h1>Register</h1>
-        <input type="email" placeholder="Email" required></input>
-        <input type="password" placeholder="Password" required></input>
-        <input type="password" placeholder="Confirm Password" required></input>
-        <input type="submit" value="Register" />
-    </form>
-    </div>
-);
+const Signup = () => {
+    
+    function submitHandler(event) {
+        event.preventDefault();
+        console.log(event.target[0].value)
+        console.log(event.target[1].value)
+        console.log(event.target[2].value)
+    }
+
+    return(
+        <div class={style.signup}>
+            <form onSubmit={(event) => submitHandler(event)}>
+                <h1>Register</h1>
+                <input type="email" placeholder="Email" required></input>
+                <input type="password" placeholder="Password" required></input>
+                <input type="password" placeholder="Confirm Password" required></input>
+                <input type="submit" value="Register" />
+            </form>
+        </div>
+    );
+};
 
 export default Signup;
